ab6f48358ca76eac2e87fa26f2744b6a01483982
Simon Livshits <simonlivshits17@solonschools.net>
2016-10-01 15:52:50 -0400

    created test files

--- js/headerTEST.js
+++ js/headerTEST.js
@@ -4,8 +4,8 @@ var bgcolor = "#a1e8d9"; //This is the background color
 
 var headerv = "partial"; //Header shape, partial or full
 
-var smfacebook = "http://www.facebook.com/pages/Lyndhurst-Community-Presbyterian-Church/157862024230469"; // Social Media - Facebook link
-var smtwitter = "http://www.twitter.com/GoBoldlyLCPC"; // Social Media - Twitter link
+//var smfacebook = "http://www.facebook.com/pages/Lyndhurst-Community-Presbyterian-Church/157862024230469"; // Social Media - Facebook link
+//var smtwitter = "http://www.twitter.com/GoBoldlyLCPC"; // Social Media - Twitter link
 
 // DROPDOWNS
 dropdown1Name = "Who We Are";
@@ -389,6 +389,12 @@ document.write('              <span style="background-color: #000;" class="icon-
 document.write('              <span style="background-color: #000;" class="icon-bar"></span>');
 document.write('              <span style="background-color: #000;" class="icon-bar"></span>');
 document.write('            </button>');
+document.write('			<div class="socialmediasmall">');
+document.write('		   	  <a class="navbar-brand" href="https://www.facebook.com/pages/Lyndhurst-Community-Presbyterian-Church/157862024230469"><img src="Image/icon/facebook.svg" class="socialMedia" > </a></li>');
+document.write('			  <a class="navbar-brand" href="https://www.twitter.com/GoBoldlyLCPC"><img src="Image/icon/twitter.svg" class="socialMedia"></a></li>');
+document.write('			  <a class="navbar-brand" href="https://www.facebook.com/pages/Lyndhurst-Community-Presbyterian-Church/157862024230469"><img src="Image/icon/instagram.svg" class="socialMedia"></a></li>');
+document.write('			</div>');
+
 document.write(' 			 <a class="navbar-brand" href="index.html"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a> ');
 document.write('          </div>');
 document.write('          <div id="navbar" class="navbar-collapse collapse">');
@@ -452,16 +458,11 @@ document.write('              </li>');
 document.write('            </ul>');
 
 
-
-
-
-
-
-document.write('            <ul class="nav navbar-nav navbar-right u2smicon">');
+/* document.write('            <ul class="nav navbar-nav navbar-right u2smicon">');
 document.write('              <li><a class="navbar-brand" target="_blank" href="' + smfacebook + '"> <svg width="20" height="20" viewBox="0 0 512 512"><path d="m393 37c23 0 42 8 58 24 16 16 24 35 24 58l0 274c0 23-8 42-24 58-16 16-35 24-58 24l-54 0 0-170 57 0 9-66-66 0 0-42c0-11 3-19 7-24 5-5 13-8 26-8l35 0 0-60c-12-1-29-2-51-2-26 0-46 7-62 23-15 15-23 36-23 64l0 49-57 0 0 66 57 0 0 170-152 0c-23 0-42-8-58-24-16-16-24-35-24-58l0-274c0-23 8-42 24-58 16-16 35-24 58-24z"></path></svg> </a></li>');
 document.write('              <li>&nbsp; &nbsp; &nbsp;</li>');
 document.write('              <li><a class="navbar-brand" target="_blank" href="' + smtwitter + '" > <svg width="20" height="20" viewBox="0 0 512 512"><path d="m402 174c-10 5-22 8-34 10 13-8 22-19 26-33-12 7-25 12-38 14-12-12-26-19-44-19-16 0-30 6-42 18-12 12-18 26-18 42 0 6 1 10 2 14-25-1-48-8-69-19-22-11-40-25-55-44-6 10-9 20-9 30 0 22 9 39 26 50-9 0-18-2-28-7l0 1c0 14 5 27 14 38 10 11 21 18 35 20-5 2-10 3-14 3-3 0-6-1-11-1 4 12 11 21 21 29 10 8 22 12 35 12-23 17-47 26-75 26-5 0-10 0-14-1 28 18 59 27 92 27 21 0 41-3 60-10 18-7 34-16 48-27 13-12 25-25 34-39 10-15 17-31 22-47 4-16 7-32 7-48 0-3-1-6-1-8 12-8 22-19 30-31z m73-55l0 274c0 23-8 42-24 58-16 16-35 24-58 24l-274 0c-23 0-42-8-58-24-16-16-24-35-24-58l0-274c0-23 8-42 24-58 16-16 35-24 58-24l274 0c23 0 42 8 58 24 16 16 24 35 24 58z"></path></svg> </a></li>');
-document.write('            </ul>');
+document.write('            </ul>'); */
 document.write('          </div><!--/.nav-collapse -->');
 document.write('        </div><!--/.container-fluid -->');
 document.write('      </nav>');
