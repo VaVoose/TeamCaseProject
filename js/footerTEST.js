6aed9f1c20a524543cb244bd68d179f461076cde
Simon Livshits <simonlivshits17@solonschools.net>
2016-10-01 20:19:48 -0400

    Moved a necessery script command from footer js to index html due to a 	possible error from poor internet connections, which would have 	caused the bootstrap javascript plugins to fail.
    Signed-off-by: Simon Livshits <simonlivshits17@solonschools.net>

--- js/footerTEST.js
+++ js/footerTEST.js
@@ -1,8 +1,6 @@
 document.write('		<footer class="footer">'); 
 document.write('			<p class="footer-text"> 5312 Mayfield Road - Lyndhurst, OH 44124 &nbsp; &nbsp; &nbsp; &nbsp; P: 440.442.2331 &nbsp; &nbsp; &nbsp; &nbsp; info@lyndhurstpresbyterian.org </p>'); 
 document.write('		</footer>'); 
-document.write('		<!-- jQuery (necessary for Bootstraps JavaScript plugins) -->'); 
-document.write('		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>'); 
 document.write('		<!-- Include all compiled plugins (below), or include individual files as needed -->'); 
 document.write('		<script src="js/bootstrap.min.js"></script>'); 
 document.write('	</body> '); 
